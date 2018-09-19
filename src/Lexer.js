7de51d303215a0d7036875677c162f6d1689efc5
Iain Shorter <iainshorter@gmail.com>
2018-09-19 22:40:22 +0100

    Update Lexer.js

--- src/Lexer.js
+++ src/Lexer.js
@@ -1,3 +1,6 @@
+import Trie from "./Trie.js";
+import TextBuffer from "./TextBuffer.js";
+
 const STAR = "*";
 const QUOTE = "\"";
 const SINGLE_QUOTE = "\'";
@@ -54,87 +57,6 @@ function isWhitespace(ch) {
     return /^\s$/.test(ch);
 }
 
-function* Lexer(characters, buffer = new TextBuffer, nested = false) {
-    const stack = [];
-    let previousToken = null;
-
-    for (const ch of characters) {
-        let token = null;
-        if (isLetter(ch)) {
-            characters.back();
-            token = lexIdentifier(characters, buffer);
-        } else if (isNumber(ch)) {
-            characters.back();
-            token = lexNumber(characters, buffer);
-        } else if (ch == QUOTE || ch == SINGLE_QUOTE) {
-            characters.back();
-            token = lexString(characters, ch, buffer);
-        } else if (ch === TEMP_QUOTE) {
-            characters.back();
-
-            const tokenIterator = lexTemplateliteral(characters, buffer);
-
-            // important that we keep track of the last token
-            for (const t of tokenIterator) {
-                token = t;
-                yield t;
-            }
-        } else if (isSymbol(ch)) {
-            characters.back();
-            const regexAllowed = ch === SLASH && regexTest(token);
-            token = lexSymbol(characters, buffer, regexAllowed);
-        } else if (!isWhitespace(ch)) {
-            throw new Error(`Unknown character ${ch}`);
-        }
-
-        // track bracket stack here, allows extra verification
-        // and aids template literals expression lexing
-
-        if (token && token.type === "symbol") {
-            switch (token.value) {
-                case "{":
-                case "[":
-                case "(":
-                    stack.push(token);
-                    break;
-                case "}":
-                case "]":
-                case ")":
-                    const prev = stack.pop();
-
-                    if (!prev) {
-                        if (nested) {
-                            // special case for template literals
-                            // an unmatched brace indicates the end
-                            // of the expression, so return
-                            if (token.value != "}")
-                                throw new Error("unmatched bracket");
-                            else {
-                                characters.back(); // push the brace back
-                                return;
-                            }
-                        }
-
-                        throw new Error("unmatched bracket");
-                    }
-
-                    if (prev.value === "(" && ")" != token.value)
-                        throw new Error("mismatched bracket");
-                    if (prev.value === "[" && "]" != token.value)
-                        throw new Error("mismatched bracket");
-                    if (prev.value === "{" && "}" != token.value)
-                        throw new Error("mismatched bracket");
-                    break;
-            }
-        }
-
-        previousToken = token;
-
-        if (token)
-            yield token;
-    }
-}
-
 function regexTest(token) {
     if (!token)
         return true;
@@ -165,7 +87,7 @@ function* lexTemplateliteral(characters, buffer) {
                 value: string
             };
 
-            yield* Lexer(characters, buffer, true);
+            yield* lexer(characters, buffer, true);
 
             position = characters.position();
             characters.next(); // consume closing brace
@@ -368,3 +290,86 @@ function lexRegex(characters, buffer, position) {
         value: statement
     };
 }
+
+function* lexer(characters, buffer = new TextBuffer, nested = false) {
+    const stack = [];
+    let previousToken = null;
+
+    for (const ch of characters) {
+        let token = null;
+        if (isLetter(ch)) {
+            characters.back();
+            token = lexIdentifier(characters, buffer);
+        } else if (isNumber(ch)) {
+            characters.back();
+            token = lexNumber(characters, buffer);
+        } else if (ch == QUOTE || ch == SINGLE_QUOTE) {
+            characters.back();
+            token = lexString(characters, ch, buffer);
+        } else if (ch === TEMP_QUOTE) {
+            characters.back();
+
+            const tokenIterator = lexTemplateliteral(characters, buffer);
+
+            // important that we keep track of the last token
+            for (const t of tokenIterator) {
+                token = t;
+                yield t;
+            }
+        } else if (isSymbol(ch)) {
+            characters.back();
+            const regexAllowed = ch === SLASH && regexTest(token);
+            token = lexSymbol(characters, buffer, regexAllowed);
+        } else if (!isWhitespace(ch)) {
+            throw new Error(`Unknown character ${ch}`);
+        }
+
+        // track bracket stack here, allows extra verification
+        // and aids template literals expression lexing
+
+        if (token && token.type === "symbol") {
+            switch (token.value) {
+                case "{":
+                case "[":
+                case "(":
+                    stack.push(token);
+                    break;
+                case "}":
+                case "]":
+                case ")":
+                    const prev = stack.pop();
+
+                    if (!prev) {
+                        if (nested) {
+                            // special case for template literals
+                            // an unmatched brace indicates the end
+                            // of the expression, so return
+                            if (token.value != "}")
+                                throw new Error("unmatched bracket");
+                            else {
+                                characters.back(); // push the brace back
+                                return;
+                            }
+                        }
+
+                        throw new Error("unmatched bracket");
+                    }
+
+                    if (prev.value === "(" && ")" != token.value)
+                        throw new Error("mismatched bracket");
+                    if (prev.value === "[" && "]" != token.value)
+                        throw new Error("mismatched bracket");
+                    if (prev.value === "{" && "}" != token.value)
+                        throw new Error("mismatched bracket");
+                    break;
+            }
+        }
+
+        previousToken = token;
+
+        if (token)
+            yield token;
+    }
+}
+
+export default lexer;
