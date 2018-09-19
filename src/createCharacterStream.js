f2400178a6cf2384d8e0b90e3c187254458778b8
Iain Shorter <iainshorter@gmail.com>
2018-09-19 22:32:50 +0100

    Create createCharacterStream.js

---
+++ src/createCharacterStream.js
@@ -0,0 +1,45 @@
+export default function createCharacterStream (str) {
+    let i = 0;
+    const arr = Array.from(str);
+    const l = arr.length;
+
+    let prev = [0, 0]
+    let pos = [0, 0];
+
+    return {
+        next() {
+            const ch = arr[i++];
+
+            prev[0] = pos[0];
+            prev[1] = pos[1];
+
+            if (ch === NEWLINE) {
+                pos[0]++;
+                pos[1] = 0;
+            } else
+                pos[1]++;
+
+            return {
+                value: ch,
+                done: i > l
+            }
+        },
+        position() {
+            return pos.slice(0);
+        },
+        peek() {
+            return arr[i];
+        },
+        peekNext() {
+            return arr[i + 1];
+        },
+        back() {
+            pos[0] = prev[0];
+            pos[1] = prev[1];
+            i = i <= 0 ? 0 : i - 1;
+        },
+        [Symbol.iterator]() {
+            return this;
+        }
+    };
+}
