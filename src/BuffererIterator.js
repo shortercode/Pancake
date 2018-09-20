ce306d28a73348a654c7422a7cbb1a2cc6ca8143
Iain Shorter <iainshorter@gmail.com>
2018-09-20 20:26:02 +0100

    Create BuffererIterator.js

---
+++ src/BuffererIterator.js
@@ -0,0 +1,41 @@
+/*
+    Wrapper for an iterator/generator that has a persistent state and
+    buffering to provide 2 value lookahead, 1 value lookbehind and step back
+*/
+
+export default function BufferedIterator (itr) {
+    let previous = null;
+    let current = itr.next();
+    let next = itr.next();
+    let future = null;
+
+    return {
+        next () {
+            previous = current;
+            current = next;
+            next = future || itr.next();
+            future = null;
+
+            return previous;
+        },
+        back () {
+            future = next;
+            next = current;
+            current = previous;
+            previous = null;
+            return current;
+        },
+        previous () {
+            return previous && previous.value;
+        },
+        peek () {
+            return current.value;
+        },
+        peekNext () {
+            return next.value;
+        },
+        [Symbol.iterator] () {
+            return this;
+        }
+    }
+}
