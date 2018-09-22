3c5a089c6430e37930e433cf5de1cd8d241459f2
shortercode <iainshorter@Iains-MacBook-Pro.local>
2018-09-22 19:18:12 +0100

    initial declaration parsing

--- src/Parser.js
+++ src/Parser.js
@@ -1,7 +1,39 @@
 import BufferedIterator from "./BuffererIterator";
 
-function* parse (tokens) {
+function parseExpression(tokens) {
 
+}
+
+function parseClass(tokens) {
+
+}
+
+function parseVariable(tokens) {
+
+}
+
+function parseFunction(tokens) {
+    
+}
+
+const decParselets = new Map;
+
+decParselets.set("var", parseVariable);
+decParselets.set("let", parseVariable);
+decParselets.set("const", parseVariable);
+decParselets.set("class", parseClass);
+
+function* parse (tokens) {
+    for (const token of tokens) {
+        const parselet = token.type === "identifier" && decParselets.get(token.value);
+
+        if (!parselet) {
+            tokens.back();
+            yield parseExpression(tokens);
+        }
+        else
+            yield parselet(token);
+    }
 }
 
 export default function (tokens) {
